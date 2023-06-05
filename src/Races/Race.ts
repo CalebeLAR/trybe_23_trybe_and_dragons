0a9e1183cf0112433f07ce8f9ca257345664bcf7
calebeLAR <ca6ele@gmail.com>
2023-06-05 15:34:29 -0400

    req01: refatora Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,13 +1,13 @@
 abstract class Race {
-  constructor(private _name:string, private _dexterity:number) {}
+  constructor(readonly name:string, readonly dexterity:number) {}
 
-  get name():string {
-    return this._name;
-  }
+  // get name():string {
+  //   return this._name;
+  // }
   
-  get dexterity():number {
-    return this._dexterity;
-  }
+  // get dexterity():number {
+  //   return this._dexterity;
+  // }
   
   abstract get maxLifePoints():number;
 
